18a7910041381a9181fd3fced1ca856d42034e28
tuanhung <mac@Megalodon.local>
2020-05-26 23:28:28 +0700

    seting

--- source_triet_hoc/settings.py
+++ source_triet_hoc/settings.py
@@ -16,20 +16,20 @@ NEWSPIDER_MODULE = 'source_triet_hoc.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'source_triet_hoc (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+# ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 1
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 #DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 1
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
@@ -39,34 +39,34 @@ ROBOTSTXT_OBEY = True
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
+# DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
-#}
+# }
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
+# SPIDER_MIDDLEWARES = {
 #    'source_triet_hoc.middlewares.SourceTrietHocSpiderMiddleware': 543,
-#}
+# }
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
+# DOWNLOADER_MIDDLEWARES = {
 #    'source_triet_hoc.middlewares.SourceTrietHocDownloaderMiddleware': 543,
-#}
+# }
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
+# EXTENSIONS = {
 #    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
+# }
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
+# ITEM_PIPELINES = {
 #    'source_triet_hoc.pipelines.SourceTrietHocPipeline': 300,
-#}
+# }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
@@ -79,12 +79,12 @@ ROBOTSTXT_OBEY = True
 # each remote server
 #AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+AUTOTHROTTLE_DEBUG = True
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = 'httpcache'
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+HTTPCACHE_ENABLED = True
+HTTPCACHE_EXPIRATION_SECS = 0
+HTTPCACHE_DIR = 'httpcache'
+HTTPCACHE_IGNORE_HTTP_CODES = []
+HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
--- source_triet_hoc/spiders/triethoc_spider.py
+++ source_triet_hoc/spiders/triethoc_spider.py
@@ -4,8 +4,59 @@ import scrapy
 
 class TriethocSpiderSpider(scrapy.Spider):
     name = 'triethoc_spider'
-    allowed_domains = ['triethocduongpho.com']
-    start_urls = ['http://triethocduongpho.com/']
+    allowed_domains = ['triethocduongpho.net']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.start_urls = self.gen_year()
+
+    def gen_year(self):
+        domain = "triethoduongpho.net"
+        domains = []
+        year_start = 2013
+        year_end = 2021
+        years = range(2013, 2020, 1)
+        for year in years:
+            domains.append(f"{domain}/{year}/")
+
+        return domains
+
+    def start_requests(self):
+        for url in self.start_urls:
+
+            yield scrapy.Request(f'https://{url}', self.parse)
+
+    def gen_page(self, total_page, response_current):
+        list_page = []
+        for page in range(1, total_page + 1, 1):
+            list_page.append(f"{response_current}/page/{page}")
+
+        return list_page
 
     def parse(self, response):
-        pass
+        next_page = response.xpath(
+            "//div[@class='pagination']/a/text()").extract()
+        total_page = int(next_page[-1])
+        list_page = self.gen_page(total_page, response.url)
+
+        for item_page in list_page:
+            yield scrapy.Request(item_page, self.parse_list)
+
+    def parse_list(self, response):
+        list_la = response.xpath('//article/header/h2/a/@href').extract()
+        for list_la_item in list_la:
+            yield scrapy.Request(list_la_item, self.parse_detail)
+
+    def parse_detail(self, response):
+        item = {}
+        title = response.xpath('//title/text()').extract_first()
+        content = response.xpath(
+            "//div[@class='entry-content']/div[1]").extract_first()
+        try:
+            content = content.split("<hr>")[0]
+        except:
+            pass
+
+        item["title"] = title
+        item["content"] = content
+        yield item
